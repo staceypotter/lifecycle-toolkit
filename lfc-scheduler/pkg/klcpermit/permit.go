50bdc50919b4ef7c2befcf4b8efd85f330299e1f
RealAnna <anna.reale@dynatrace.com>
2022-09-20 09:54:32 +0200

    added service crd manager
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- lfc-scheduler/pkg/klcpermit/permit.go
+++ lfc-scheduler/pkg/klcpermit/permit.go
@@ -5,9 +5,11 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
+	clientcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
-	ref "k8s.io/client-go/tools/reference"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	"time"
@@ -21,6 +23,7 @@ const (
 // Permit is a plugin that implements a wait for pre-deployment checks
 type Permit struct {
 	handler    framework.Handle
+	recorder   record.EventRecorder
 	svcManager *ServiceManager
 }
 
@@ -35,41 +38,24 @@ func (pl *Permit) Permit(ctx context.Context, state *framework.CycleState, p *v1
 
 	klog.InfoS("[Keptn Permit Plugin] waiting for pre-deployment checks on", p.GetObjectMeta().GetName())
 
-	err := pl.sendEvent(p, "test", "started", "Waiting for pre-deployment checks")
-	if err != nil {
-		return framework.NewStatus(framework.Error), 0 * time.Second
-	}
+	pl.recorder.Event(p, v1.EventTypeNormal, "SomeReason", "Waiting Pre-Deployment")
 
 	switch pl.svcManager.Permit(ctx, p) {
 
 	case Wait:
-		klog.Infof("[Keptn Permit Plugin] waiting for pre-deployment checks on", p.GetObjectMeta().GetName())
-		return framework.NewStatus(framework.Wait), 30 * time.Second
+		klog.InfoS("[Keptn Permit Plugin] waiting for pre-deployment checks on", p.GetObjectMeta().GetName())
+		return framework.NewStatus(framework.Wait), 5 * time.Second
 	case Failure:
-		klog.Infof("[Keptn Permit Plugin] failed pre-deployment checks on", p.GetObjectMeta().GetName())
+		klog.InfoS("[Keptn Permit Plugin] failed pre-deployment checks on", p.GetObjectMeta().GetName())
 		return framework.NewStatus(framework.Error), 0 * time.Second
 	case Success:
-		klog.Infof("[Keptn Permit Plugin] passed pre-deployment checks on", p.GetObjectMeta().GetName())
+		klog.InfoS("[Keptn Permit Plugin] passed pre-deployment checks on", p.GetObjectMeta().GetName())
 		return framework.NewStatus(framework.Success), 0 * time.Second
 	default:
-		klog.Infof("[Keptn Permit Plugin] unknown status of pre-deployment checks for", p.GetObjectMeta().GetName())
-		return framework.NewStatus(framework.Wait), 30 * time.Second //TODO what makes sense here?
+		klog.InfoS("[Keptn Permit Plugin] unknown status of pre-deployment checks for", p.GetObjectMeta().GetName())
+		return framework.NewStatus(framework.Wait), 5 * time.Second //TODO what makes sense here?
 	}
 
-}
-
-func (pl *Permit) sendEvent(p *v1.Pod, reason string, action string, note string) error {
-	regarding, err := ref.GetPartialReference(scheme.Scheme, p, ".spec.containers[1]")
-	if err != nil {
-		return err
-	}
-
-	related, err := ref.GetPartialReference(scheme.Scheme, p, ".spec.containers[0]")
-	if err != nil {
-		return err
-	}
-	pl.handler.EventRecorder().Eventf(regarding, related, v1.EventTypeNormal, reason, action, note)
-	return nil
 }
 
 // New initializes a new plugin and returns it.
@@ -80,9 +66,25 @@ func New(_ runtime.Object, h framework.Handle) (framework.Plugin, error) {
 	}
 
 	return &Permit{
+		recorder:   setupRecorder(),
 		svcManager: NewServiceManager(client),
 		handler:    h,
 	}, nil
+}
+
+func setupRecorder() record.EventRecorder {
+
+	var config *rest.Config
+	config, _ = rest.InClusterConfig()
+
+	clientset, _ := kubernetes.NewForConfig(config)
+	eventSink := &clientcorev1.EventSinkImpl{
+		Interface: clientset.CoreV1().Events(v1.NamespaceAll),
+	}
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartRecordingToSink(eventSink)
+	r := eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "keptn-scheduler"})
+	return r
 }
 
 func newClient() (dynamic.Interface, error) {
--- lfc-scheduler/pkg/klcpermit/service_manager.go
+++ lfc-scheduler/pkg/klcpermit/service_manager.go
@@ -6,29 +6,29 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	types "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/klog/v2"
 )
 
-var serviceResource = schema.GroupVersionResource{Group: "lifecycle.keptn.sh", Version: "v1alpha1", Resource: "serviceruns"} //TODO change this resource name with workloadinstance and eventually appinstance :)
+var serviceResource = schema.GroupVersionResource{Group: "lifecycle.keptn.sh", Version: "v1alpha1", Resource: "servicerun"}
 
 type Status string
 
 const (
-	ServiceRunStatusNotSpecified Status = "Service run status not specified"
-	ServiceRunNotFound           Status = "Service run not found"
-	Success                      Status = "Success"
-	Failure                      Status = "Failure"
-	Wait                         Status = "Wait"
+	ServiceRunNotSpecified Status = "Service run not specified"
+	ServiceRunNotFound     Status = "Service run not found"
+	Success                Status = "Success"
+	Failure                Status = "Failure"
+	Wait                   Status = "Wait"
 )
 
 const (
 	// ServiceRunPending means the application has been accepted by the system, but one or more of its
 	// serviceRuns has not been started.
 	ServiceRunPending string = "Pending"
-	// ServiceRunRunning means that serviceRun has been started.
+	// ServiceRunRunning means that all of the serviceRuns have been started.
 	ServiceRunRunning string = "Running"
-	// ServiceRunSucceeded means that serviceRun has been finished successfully.
+	// ServiceRunSucceeded means that all of the serviceRuns have been finished successfully.
 	ServiceRunSucceeded string = "Succeeded"
 	// ServiceRunFailed means that one or more pre-deployment checks was not successful and terminated.
 	ServiceRunFailed string = "Failed"
@@ -44,52 +44,64 @@ type ServiceManager struct {
 	dynamicClient dynamic.Interface
 }
 
-func NewServiceManager(d dynamic.Interface) *ServiceManager {
+func NewServiceManager(dy dynamic.Interface) *ServiceManager {
 	sMgr := &ServiceManager{
-		dynamicClient: d,
+		dynamicClient: dy,
 	}
 	return sMgr
 }
 
-func (sMgr *ServiceManager) Permit(ctx context.Context, pod *corev1.Pod) Status {
-	//List service run CRDs
-	name := GetCRDName(pod)
-	crd, err := sMgr.GetCRD(ctx, metav1.NamespaceDefault, name)
+func (sMgr *ServiceManager) Permit(ctx context.Context, pod *corev1.Pod) Status { //This is to not have tight coupling with CRD resource
+
+	// list resources here
+	services, err := sMgr.ListServiceRun(ctx, pod.Namespace)
 
 	if err != nil {
-		klog.Infof("[Keptn Permit Plugin] could not find service crd %s, err:%s", name, err.Error())
-		return ServiceRunNotFound
+		return ServiceRunNotSpecified
 	}
-	//check CRD status
-	phase, found, err := unstructured.NestedString(crd.UnstructuredContent(), "status", "phase")
-	klog.Infof("[Keptn Permit Plugin] service crd %s, found %s with phase %s ", crd, found, phase)
-	if err == nil && found {
-		switch phase {
-		case ServiceRunPending:
-			return Wait
-		case ServiceRunFailed:
-			return Failure
-		case ServiceRunSucceeded:
-			return Success
-		case ServiceRunRunning:
-			return Wait
-		case ServiceRunUnknown:
-			return Wait
+	owner := GetPodOwner(pod)
+	for _, s := range services {
+		//get spec!
+		replicaRef, found, err := unstructured.NestedFieldCopy(s.UnstructuredContent(), "spec", "replicasetref")
+
+		if err == nil && found && replicaRef == owner {
+			phase, found, err := unstructured.NestedString(s.UnstructuredContent(), "status", "phase")
+			if err == nil && found {
+				switch phase {
+				case ServiceRunPending:
+					return Wait
+				case ServiceRunFailed:
+					return Failure
+				case ServiceRunSucceeded:
+					return Success
+				case ServiceRunRunning:
+					return Wait
+				case ServiceRunUnknown:
+					return Wait
+				}
+
+			}
 		}
-
 	}
-	return ServiceRunStatusNotSpecified
+
+	return ServiceRunNotFound
 }
 
-//GetCRD returns unstructured to avoid tight coupling with the CRD resource
-func (sMgr *ServiceManager) GetCRD(ctx context.Context, namespace string, name string) (*unstructured.Unstructured, error) {
-	// GET /apis/lifecycle.keptn.sh/v1/namespaces/{namespace}/servicerun/name
-	return sMgr.dynamicClient.Resource(serviceResource).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
+func (sMgr *ServiceManager) ListServiceRun(ctx context.Context, namespace string) ([]unstructured.Unstructured, error) {
+	// GET /apis/lifecycle.keptn.sh/v1/namespaces/{namespace}/servicerun/
+	list, err := sMgr.dynamicClient.Resource(serviceResource).Namespace(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return list.Items, nil
 }
 
-func GetCRDName(pod *corev1.Pod) string {
-	application := pod.Annotations["keptn.sh/application"]
-	service := pod.Annotations["keptn.sh/service"]
-	version := pod.Annotations["keptn.sh/version"]
-	return application + "-" + service + "-" + version
+func GetPodOwner(pod *corev1.Pod) types.UID {
+	for _, owner := range pod.OwnerReferences {
+		if owner.Kind == "ReplicaSet" {
+			return owner.UID
+		}
+	}
+	return ""
 }
